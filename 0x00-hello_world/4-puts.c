9b2aa9b1a59f6b916732e083c1c3be47c0b396c0
MerVitz <amakaluvitalis202@gmail.com>
2022-06-30 17:36:02 -0700

    7

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,27 @@
-#include <studio.h>
+#include <stdio.h>
+
+
 
 /**
- * main - Entry point
- * print word to output
- * Return: Always 0 (Success)
- */
+ *
+ *  * main - Entry point
+ *
+ *   * print word to output
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+
 
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}      	
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
+
+
