62963268ee1e165b2a335f7e37f173b5d94e4349
MerVitz <amakaluvitalis202@gmail.com>
2022-07-11 18:51:48 -0700

    Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
-/*
- * A Function taking a pointer to int
- * Updating the value to 98 
+/**
+ * reset_to_98 - reset the value of a pointer to 98
+ * @n: integer pointer
+ * Return: void
  */
-
  void reset_to_98(int *n)
 {
 	*n = 98;
