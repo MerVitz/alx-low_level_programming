fc583d632cd215b70e06285e11529d048fbf24d3
MerVitz <amakaluvitalis202@gmail.com>
2023-10-19 06:04:49 -0700

    Ho

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
+include <stdio.h>
+void first(void) __attribute__ ((constructor));
 /**
  * bfr_main - function to execute before main
  *
  */
-void __attribute__ ((constructor)) bfr_main()
+void first(void)
 {
 printf("You're beat! and yet, you must allow");
 printf(",\nI bore my house upon my back !\n ");
