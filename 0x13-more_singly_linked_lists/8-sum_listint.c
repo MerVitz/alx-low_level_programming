75a5b569a28da367d02c58d76fe0ead567229f69
MerVitz <amakaluvitalis202@gmail.com>
2023-10-25 18:33:37 -0700

    Sum List

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -8,15 +8,15 @@
  */
 int sum_listint(listint_t *head)
 {
- int sum = 0;
- listint_t *temp = head;
+int sum = 0;
+listint_t *temp = head;
 
- while (temp)
+while (temp)
  {
- sum += temp->n;
- temp = temp->next;
- }
+sum += temp->n;
+temp = temp->next;
+}
 
- return (sum);
+return (sum);
 }
 
