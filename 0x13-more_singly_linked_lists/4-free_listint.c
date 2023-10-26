30844cb7e5520cde15b64371128de01d1aa0aa8d
MerVitz <amakaluvitalis202@gmail.com>
2023-10-25 18:14:32 -0700

    Crazzy

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,18 +1,16 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * free_listint - frees a linked list
- * @head: listint_t list to be freed
+ * free_listint - frees a listint_t list
+ *
+ * @head: node
  */
 void free_listint(listint_t *head)
 {
- listint_t *temp;
-
- while (head)
- {
- temp = head->next;
- free(head);
- head = temp;
- }
+if (head == NULL)
+{
+return;
 }
-
+free_listint(head->next);
+free(head);
+}
