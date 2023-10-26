23954b413158539348a4a3eda6d46774bb0d2400
MerVitz <amakaluvitalis202@gmail.com>
2023-10-25 18:09:59 -0700

    Implicit declaration of a malloc function

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,35 +1,32 @@
 #include "lists.h"
-
+#include <stdlib.h>
 /**
- * add_nodeint_end - adds a node at the end of a linked list
- * @head: pointer to the first element in the list
- * @n: data to insert in the new element
+ * add_nodeint_end - adds a new node at the end of a listint_t list.
  *
- * Return: pointer to the new node, or NULL if it fails
+ * @head: current position in the list
+ * @n: int to add to the list
+ * Return: listint_t*
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
- listint_t *new;
- listint_t *temp = *head;
-
- new = malloc(sizeof(listint_t));
- if (!new)
- return (NULL);
-
- new->n = n;
- new->next = NULL;
-
- if (*head == NULL)
- {
- *head = new;
- return (new);
- }
-
- while (temp->next)
- temp = temp->next;
-
- temp->next = new;
-
- return (new);
+listint_t *new, *c;
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+{
+return (NULL);
 }
-
+new->n = n;
+new->next = NULL;
+if (*head == NULL)
+{
+*head = new;
+return (*head);
+}
+c = *head;
+while (c->next != NULL)
+{
+c = c->next;
+}
+c->next = new;
+return (*head);
+}
