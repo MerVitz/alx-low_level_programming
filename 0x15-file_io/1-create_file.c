2567fce22101e0a82a563a7627887385ba9b2654
MerVitz <amakaluvitalis202@gmail.com>
2023-10-30 13:00:23 -0700

    UUmmmh

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,7 +5,7 @@
  * @filename: filename.
  * @text_content: content writed in the file.
  *
- * Returns: 1 if it success, else -1
+ * Return: 1 if it success, else -1
  */
 int create_file(const char *filename, char *text_content)
 {
