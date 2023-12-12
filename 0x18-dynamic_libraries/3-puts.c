910beb1003c00f65588ebea226c093c74f5c39db
MerVitz <amakaluvitalis202@gmail.com>
2023-12-11 18:52:51 -0800

    Yeah

--- 0x18-dynamic_libraries/3-puts.c
+++ 0x18-dynamic_libraries/3-puts.c
@@ -3,9 +3,12 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-
+/**
+ * _puts - It is used to display strings to the screen
+ * @s: a variable returned
+ *
+ **/
 void _puts(char *s)
 {
-    puts(s);
+puts(s);
 }
-
--- 0x18-dynamic_libraries/README.md
+++ 0x18-dynamic_libraries/README.md
@@ -1,1 +1,5 @@
+##Dynamic Libraries
+gcc -fPIC -c *.c will help generate the object files for all the .c files
 Dynamic library is a shared kind of library
+Creating a Dynaic Library:
+# gcc -shared -o librayname.so *.o
